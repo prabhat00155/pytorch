5c1f63859171e64a22726776c0887f1436ee3fb7
Howard Huang <howardhuang@fb.com>
2021-04-13 14:26:38 +0000

    add rpc.barrier
    ghstack-source-id: 972918927ee8638a774924613bf4146b5ea58bdc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/53423

--- torch/distributed/rpc/__init__.py
+++ torch/distributed/rpc/__init__.py
@@ -12,6 +12,7 @@ logger = logging.getLogger(__name__)
 
 _init_counter = 0
 _init_counter_lock = threading.Lock()
+_rpc_barrier_workers_lock = threading.Lock()
 
 def is_available():
     return hasattr(torch._C, "_rpc_init")
@@ -196,6 +197,27 @@ if is_available():
         _set_profiler_node_id(rank)
         # Initialize RPC.
         _init_rpc_backend(backend, store, name, rank, world_size, rpc_backend_options)
+
+
+    @api._require_initialized
+    def barrier(worker_names=None):
+        r"""
+        Synchronizes local and remote RPC processes.
+
+        This will block until all local and remote RPC processes specified under worker_names
+        reach this method to wait for all outstanding work to complete.
+
+        Args:
+            worker_names (List[str], optional): The set of workers to synchronize. If ``None``, the
+            set will be all workers. Default is ``None``.
+
+        """
+        # When specifying a set of workers, only allow one barrier at a time
+        if worker_names:
+            with _rpc_barrier_workers_lock:
+                api._all_gather(None, set(worker_names))
+        else:
+            api._all_gather(None)
 
 
     def _validate_rpc_args(backend, store, name, rank, world_size, rpc_backend_options):
